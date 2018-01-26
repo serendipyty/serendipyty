d4dff308292c3ba9d0bc9739255008bb068da316
Filippo B <filippo.broggini@erdw.ethz.ch>
2018-01-26 17:43:07 +0100

    Add version to __init__.py

--- serendipyty/__init__.py
+++ serendipyty/__init__.py
@@ -5,3 +5,6 @@ __doc__ = """Serendipyty: a Python library for learning and teaching Geophysics
 Serendipyty is a collection of geophysical algorithms for Python.
 """
 
+# This is extracted automatically by the top-level setup.py.
+__version__ = '0.0.1'
+
